3e91430d7416b57a31dce83e859c8fdad528feb6
Kouame Gerard <gerardkouame8@gmail.com>
2018-11-30 15:11:11 +0000

    Type Variable Update...

---
+++ Interpolation String.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+var name = "Ghost Web"
+var age = 20
+var phone = 2256930632690
+"Your name is \(name)"//
+"Your name is " + name//
+"Your name is \(name), your age is \(age), and your latitude is \(phone)"
+
+print("Your name is \(name)")
+print("Your name is " + name)
+
---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Oparator Int, Float, Double
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var c = 1.1
+var d = 2.1
+var e = c + d
+
+//Operator String
+var name1 = "Kouame Gerard"
+var name2 = "Kodjo"
+var both = name1 + " and " + name2
+
+//Operator comparison
+c > 3
+c >= 3
+c > 4
+c < 4
+
+name1 == "Kouame Gerard"
+name1 != "Kouame Gerard"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
